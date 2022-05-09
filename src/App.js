55dc8fdfd3931e120ba5a9d857d7586e9b939794
Aaron Hubber <aaronhubber@gmail.com>
2022-05-09 14:37:39 +0100

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+ 
+    const [items, setItems] = useState([
+      {description: "Walk Dog", priority:"high"},
+      {description: "Clean Dishes", priority:"high"},
+      {description: "Wash Windows", priority:"low"}
+    ])
+ 
+    const [newItem, setNewItem] = useState("")
+
+
+  
+    const itemNodes = items.map((item) => { 
+      return(
+          <li><span>{item.description}</span></li> 
+      )
+  })
+
+  
+  
+    const handleItemInput = (event) =>{
+      setNewItem(event.target.value)
+    }
+  
+    const saveNewItem=((event) => {
+      event.preventDefault()
+      const copyItem = [... items]
+      copyItem.push({description: newItem})
+      setItems(copyItem)
+      setNewItem("")
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div className="List">
+
+    <h1>ToDo List</h1>
+    <hr></hr>
+
+    <form onSubmit ={saveNewItem}>
+
+    <label htmlFor="new-item">Add a new item:</label>  
+      <input id="new-item" type="text" onChange={handleItemInput} value={newItem}/>                
+      High Priority
+    <input type="radio" name="high-priority"></input>
+    Low priority
+    <input type="radio" name="low-priority"></input>
+    <input type="submit" value="Save New Item" /> 
+    </form>
+
+    <ul>
+    {itemNodes}
+    </ul>
+
+
+</div>
   );
 }
 
